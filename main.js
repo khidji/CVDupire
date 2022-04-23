11015c3e5141d6ef504cc8edc81fe63e294b52f5
khidji <dupirelucie@gmail.com>
2022-04-23 22:44:44 +0200

    finito

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// BURGER MENU 
 
 const burgerElement = document.querySelector('.burger');
 
@@ -10,3 +11,22 @@ burgerElement.addEventListener('click', () => {
   headerList.classList.toggle('visible');
 });
 
+// HEADER (STICKY CLASS)
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("menu");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
