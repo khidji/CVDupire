f18177b4aa5a2b45ab3ed43766e1c2214bfdcfcb
khidji <dupirelucie@gmail.com>
2022-04-27 13:29:10 +0200

    finito

--- main.js
+++ main.js
@@ -1,19 +1,5 @@
-// BURGER MENU 
-// Quand l'utilisateur est sur téléphone, menu "hamburger"
-
-const burgerElement = document.querySelector('.burger');
-
-burgerElement.addEventListener('click', () => {
-  const headerList = document.querySelector('.menu-liste');
-
-  burgerElement.classList.toggle('open');
-  burgerElement.classList.toggle('close');
-
-  headerList.classList.toggle('visible');
-});
-
 // HEADER (STICKY CLASS)
-// fonction quand la fenêtre arrive au niveau du menu, celui ci prend la class sticky
+// quand l'utilisateur scroll et que le haut de la page arrive au niveau du menu, celui ci prend la class sticky
 
 window.onscroll = function() {myFunction()};
 
